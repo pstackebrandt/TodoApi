782c4c736b7bb3d1390f01b74537aea17de65ccc
pstackebrandt <p.stackebrandt@immowelt.de>
2018-04-03 16:42:58 +0200

    Create Item hinzugefügt

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -37,5 +37,19 @@ namespace TodoApi.Controllers
             }
             return new ObjectResult(item);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
     }
 }
